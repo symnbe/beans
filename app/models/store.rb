eb2962c5899727c4b7fdb0dbbf9da94df8ca5b02
symnbe <ookaminingen@me.com>
2023-04-14 23:59:59 +0900

    fix

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,8 +2,6 @@ class Store < ApplicationRecord
 
   has_many :beans, dependent: :destroy
 
-  # map 機能　住所から緯度と軽度を取得
-
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
 
