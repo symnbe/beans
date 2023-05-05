85982167790304e359d22b7e159fe0ac058b6de7
symnbe <ookaminingen@me.com>
2023-05-05 23:59:59 +0900

    fix

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,7 +10,7 @@ class FavoritesController < ApplicationController
     @bean = Bean.find(params[:bean_id])
     favorite = current_user.favorites.find_by(bean_id: @bean.id)
     favorite.destroy
-    # redirect_to bean_path(bean)
   end
+
 
 end
