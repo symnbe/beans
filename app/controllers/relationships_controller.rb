71558989ee904e797ddb1404ec8f3911f1979e75
symnbe <ookaminingen@me.com>
2023-05-10 08:47:06 +0000

    [fix]following+followe_html/user_name追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -14,13 +14,13 @@ class RelationshipsController < ApplicationController
   end
 
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings
   end
 
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
+    @user = User.find(params[:user_id])
+    @users = @user.followers
   end
 
   private
