006faa90fc6ef6f899c9faa8357d90a5aae495ab
symnbe <ookaminingen@me.com>
2023-05-10 04:06:39 +0000

    [add]beans/show.html/投稿日時・コメント投稿日時の表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def formatted_time_ago(time)
+   now = Time.current
+   diff_in_seconds = (now - time).round
+
+    case diff_in_seconds
+      when 0..59
+        "#{diff_in_seconds}秒前"
+      when 60..3599
+        "#{diff_in_seconds / 60}分前"
+      when 3600..86_399
+        "#{diff_in_seconds / 3600}時間前"
+      when 86_400..604_799
+        "#{diff_in_seconds / 86_400}日前"
+      when 604_800..(now.beginning_of_day - 1.second - time).round
+        "#{diff_in_seconds / 86_400}日前"
+      else
+        time.strftime("%Y年%m月%d日")
+    end
+  end
 end
